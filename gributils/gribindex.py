b2578ae283b8fb3a0e75c4e83a35cb71a5c4fb72
Egil <egil.moller@freecode.no>
2018-09-26 10:32:23 +0200

    Possibly buggy version with a first implementation of a grib index

---
+++ gributils/gribindex.py
@@ -0,0 +1,85 @@
+import os
+import sys
+import pygrib
+import psycopg2
+import shapely.geometry
+import shapely.ops
+import shapely.affinity
+import scipy.ndimage.morphology
+import skimage.measure
+import numpy as np
+import json
+import pyproj
+import functools
+import hashlib
+import gributils.bounds
+import csv
+
+class GribIndex(object):
+    def __init__(self, db_connect_string):
+        self.conn = psycopg2.connect(db_connect_string)
+        self.cur = self.conn.cursor()
+
+    def extract_polygons(self, layer):
+        shape = gributils.bounds.bounds(layer)
+        return gributils.bounds.polygon_id(shape), shape
+
+    def add_dir(self, basedir, cb):
+        for root, dirs, files in os.walk(basedir):
+            for filename in files:
+                if not (filename.endswith(".grib") or filename.endswith(".grb")): continue
+                filepath = os.path.abspath(os.path.join(root, filename))
+                try:
+                    self.add_file(filepath)
+                except Exception as e:
+                    cb({
+                        "file": filepath,
+                        "error": e
+                        })
+
+    def load_parametermap(self, filepath):
+        for parametermap_file in (filepath + ".parametermap.csv",
+                                  os.path.join(os.path.dirname(filepath), "parametermap.csv")):
+            if os.path.exists(parametermap_file):
+                parametermap = {}
+                with open(parametermap_file) as f:
+                    for row in csv.DictReader(f):
+                        parametermap[int(row["parameter"])] = (row["name"], row["unit"])
+                return parametermap
+        return {}
+                    
+    def add_file(self, filepath):
+        parametermap = self.load_parametermap(filepath)
+        
+        cur.execute("SELECT count(*) FROM gribfiles WHERE file = %s",
+                     (filepath,))
+        if cur.fetchone()[0] > 0:
+            print("%s IGNORE" % filepath)
+            continue
+        print("%s INDEX" % filepath)
+        cur.execute("INSERT INTO gribfiles (file) VALUES (%s)",
+                     (filepath,))
+        with pygrib.open(filepath) as grbs:
+            for grb in grbs:
+                parameter_name = grb.parameterName
+                parameter_unit = grb.parameterUnits
+                if grb.parameterNumber in parametermap:
+                    parameter_name, parameter_unit = parametermap[grb.parameterNumber]
+              
+                measurementid = "%s,%s,%s" % (parameter_name, parameter_unit, grb.typeOfLevel, grb.level)
+                gridid, poly = extract_polygons(grb)
+
+                cur.execute("INSERT INTO gridareas (gridid, projparams, the_geom) VALUES (%s, %s, st_geomfromtext(%s, 4326)) ON CONFLICT DO NOTHING",
+                            (gridid, json.dumps(grb.projparams), poly.wkt))
+
+                cur.execute("""INSERT
+                                 INTO measurement (measurementid, parameterName, parameterUnit, typeOfLevel, level)
+                                 VALUES (%s, %s, %s, %s)
+                                 ON CONFLICT DO NOTHING""",
+                            (measurementid, parameter_name, parameter_unit, grb.typeOfLevel, grb.level))
+
+                cur.execute("INSERT INTO griblayers (file, measurementid, timestamp, gridid) VALUES (%s, %s, %s, %s)",
+                            (filepath, measurementid, grb.validDate, gridid))
+
+        cur.execute("COMMIT")
+
---
+++ gributils/uv.py
@@ -0,0 +1,18 @@
+import gributils.projection
+
+wgs84_geod = pyproj.Geod(ellps='WGS84')
+
+def uv_to_magnitude_azimuth(grbU, grbV):
+    proj = gributils.projection.LayerProjection(grbU)
+
+    lats1, lons1 = grbU.latlons()
+    x1, y1 = proj.project(lons1, lats1)
+
+    x2 = x1 + grbU.values * proj.dx
+    y2 = y1 + grbV.values * proj.dy
+
+    lons2, lats2 = proj.unproject(x2, y2)
+    azimuth, back, dist = wgs84_geod.inv(lons1, lats1, lons2, lats2)
+
+    magnitude = np.sqrt(grbU.values**2 + grbV.values**2)
+    return magnitude, azimuth
