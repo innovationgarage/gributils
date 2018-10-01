3a23c36d8762f6c00fbacc06a213d8090cab00ce
asadisaghar <asadisaghar@gmail.com>
2018-10-01 13:51:56 +0200

    add_dir and add_file added to the cli

--- gributils/cli.py
+++ gributils/cli.py
@@ -13,7 +13,7 @@ def main(ctx, **kw):
 def index(ctx, database, **kw):
     ctx.obj = {}
     ctx.obj['index'] = gributils.gribindex.GribIndex(database)
-
+    
 @index.command()
 @click.argument("output", type=click.Choice(['layers', 'names', 'units', 'level-types', 'levels']))
 @click.option('--timestamp', type=click_datetime.Datetime(format='%Y-%m-%d %H:%M:%S'), default=None)
@@ -34,3 +34,26 @@ def lookup(ctx, **kw):
     for result in ctx.obj["index"].lookup(**kw):
         print(", ".join(mangle(item) for item in result))
 
+@index.command()
+@click.option("--filepath", type=str)
+@click.pass_context
+def add_file(ctx, **kw):
+    def mangle(item):
+        if hasattr(item, 'strftime'):
+            return item.strftime("%Y-%m-%d %H:%M:%S")
+        return str(item)
+    ctx.obj["index"].add_file(**kw)
+
+def show_error(ctx, err):
+    print(err)
+    
+@index.command()
+@click.option("--basedir", type=str)
+@click.pass_context
+def add_dir(ctx, **kw):
+    def mangle(item):
+        if hasattr(item, 'strftime'):
+            return item.strftime("%Y-%m-%d %H:%M:%S")
+        return str(item)
+    ctx.obj["index"].add_dir(**kw, cb=show_error)
+    
