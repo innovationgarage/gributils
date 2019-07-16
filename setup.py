e341f177f7568d046a45f0dd896fea0d69efeb82
Egil <egil.moller@freecode.no>
2019-07-16 10:29:12 +0200

    Bugfix for long desc format

--- setup.py
+++ setup.py
@@ -5,19 +5,16 @@ import setuptools
 setuptools.setup(name='gributils',
       version='0.6',
       description='Utils on top of pygrib for extracting metadata and manipulating grib files',
-      long_description="""# gributils
-gributils is a set of utilities on top of pygrib for manipulating and
+      long_description="""gributils is a set of utilities on top of pygrib for manipulating and
 indexing gribfiles. Current features include:
 
 * Grib index
   * Store and query a large set (historical dataset) of grib files
   * PostGIS based
-  * Query by geographical location,, timestamp and parameter name,
-    parameter unit, level and level type.
+  * Query by geographical location,, timestamp and parameter name, parameter unit, level and level type.
   * Parameter name normalization using a simple CSV file
 * Extract polygons of areas covered with valid values in a grib layer
-* Provide accurate grid coordinates, even for grib version 1 files
-  where too low precision in Dx / Dy have resulted in a distorted grid.
+* Provide accurate grid coordinates, even for grib version 1 files where too low precision in Dx / Dy have resulted in a distorted grid.
 * Conversion from values with U and V components (e.g. for wind) to
   magnitude and degree north.
       """,
